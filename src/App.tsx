cdf1fa82bd0985309295f74ee4121b5b64089519
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-02-22 11:44:40 +0700

    [chore]: Add Apollo GraphQL and Codegen

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from "react"
+import { useQuery } from "@apollo/client"
+
+import { ExampleTokensQueryDocument } from "./generate/graphql"
 
 function App() {
+    const { data } = useQuery(ExampleTokensQueryDocument)
+    console.log("data :", data)
+
     return <div className="text-red-500">Uniswap</div>
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import "./index.css"
 import App from "./App"
+
+const client = new ApolloClient({
+    uri: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
+    cache: new InMemoryCache(),
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
     <React.StrictMode>
-        <App />
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
     </React.StrictMode>
 )
