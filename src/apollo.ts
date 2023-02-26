f12989df48361d0dba29fe440021eddf0203fc8e
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-02-26 14:58:11 +0700

    [refactor]: change default fetch-policy to network-only

--- src/apollo.ts
+++ src/apollo.ts
@@ -3,9 +3,19 @@ import { ApolloClient, InMemoryCache } from "@apollo/client"
 export const UniswapV3Client = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3",
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "network-only",
+    },
+  },
 })
 
 export const EthereumBlocksClient = new ApolloClient({
   uri: "https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks",
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      fetchPolicy: "network-only",
+    },
+  },
 })
