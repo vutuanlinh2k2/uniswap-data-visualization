d06e7468135ce087b6ee2be81ef102774f21f5a0
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2023-02-27 15:30:17 +0700

    [chore]: change header and body size

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,12 @@ function App() {
   return (
     <div>
       <Header />
-      <div className="px-32 bg-secondary pt-4 pb-12 flex flex-col gap-2">
-        <Section title="Top Tokens" component={<TopTokensTable />} />
-        <Section title="Top Pools" component={<TopPoolsTable />} />
-        <Section title="Transactions" component={<TransactionsTable />} />
+      <div className="bg-secondary pt-4 pb-12 w-full">
+        <div className="flex flex-col gap-2 m-auto max-w-screen-xl" style={{ width: "85%" }}>
+          <Section title="Top Tokens" component={<TopTokensTable />} />
+          <Section title="Top Pools" component={<TopPoolsTable />} />
+          <Section title="Transactions" component={<TransactionsTable />} />
+        </div>
       </div>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,19 +21,24 @@ const Header = () => {
         }
 
   return (
-    <header className="bg-primary flex justify-between items-center px-32 py-4">
-      <div className="flex items-center gap-1.5">
-        <img className="w-8 h-8" src="/UniswapLogo.png" />
-        <h2 className="text-white text-2xl">Uniswap Data Visualization</h2>
-      </div>
-      <button
-        disabled={isAppLoading}
-        onClick={refreshApp}
-        className="flex items-center gap-2 bg-secondary rounded-2xl p-3 hover:opacity-60 disabled:opacity-60"
+    <header className="bg-primary py-4">
+      <div
+        className="flex items-center w-full justify-between max-w-screen-xl m-auto"
+        style={{ width: "85%" }}
       >
-        <FiRefreshCcw color="#ffffff" />
-        Refresh
-      </button>
+        <div className="flex items-center gap-1.5">
+          <img className="w-8 h-8" src="/UniswapLogo.png" />
+          <h2 className="text-white text-2xl">Uniswap Data Visualization</h2>
+        </div>
+        <button
+          disabled={isAppLoading}
+          onClick={refreshApp}
+          className="flex items-center gap-2 bg-secondary rounded-2xl p-3 hover:opacity-60 disabled:opacity-60"
+        >
+          <FiRefreshCcw color="#ffffff" />
+          Refresh
+        </button>
+      </div>
     </header>
   )
 }
